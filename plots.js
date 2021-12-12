df1ddd86ec98fb5d80ffc78368a38425bdbf9ecf
jaimiesj <88955412+jaimiesj@users.noreply.github.com>
2021-12-11 21:54:18 -0500

    Updated code

--- plots.js
+++ plots.js
@@ -1,98 +1,38 @@
-function init() {
-  // Grab a reference to the dropdown select element
-  var selector = d3.select("#selDataset");
+// Sort the data array using the greekSearchResults value
+data.sort(function(a, b) {
+  return parseFloat(b.greekSearchResults) - parseFloat(a.greekSearchResults);
+});
 
-  // Use the list of sample names to populate the select options
-  d3.json("samples.json").then((data) => {
-    var sampleNames = data.names;
+// Slice the first 10 objects for plotting
+data = data.slice(0, 10);
 
-    sampleNames.forEach((sample) => {
-      selector
-        .append("option")
-        .text(sample)
-        .property("value", sample);
-    });
+// Reverse the array due to Plotly's defaults
+data = data.reverse();
 
-    // Use the first sample from the list to build the initial plots
-    var firstSample = sampleNames[0];
-    buildCharts(firstSample);
-    buildMetadata(firstSample);
-  });
-}
+// Trace1 for the Greek Data
+var trace1 = {
+  x: data.map(row => row.greekSearchResults),
+  y: data.map(row => row.greekName),
+  text: data.map(row => row.greekName),
+  name: "Greek",
+  type: "bar",
+  orientation: "h"
+};
 
-// Initialize the dashboard
-init();
+// data
+var data = [trace1];
 
-function optionChanged(newSample) {
-  // Fetch new data each time a new sample is selected
-  buildMetadata(newSample);
-  buildCharts(newSample);
-}
+// Apply the group bar mode to the layout
+var layout = {
+  title: "Greek gods search results",
+  margin: {
+    l: 100,
+    r: 100,
+    t: 100,
+    b: 100
+  }
+};
 
-// Demographics Panel 
-function buildMetadata(sample) {
-  d3.json("samples.json").then((data) => {
-    var metadata = data.metadata;
-    // Filter the data for the object with the desired sample number
-    var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
-    var result = resultArray[0];
-    // Use d3 to select the panel with id of `#sample-metadata`
-    var PANEL = d3.select("#sample-metadata");
-
-    // Use `.html("") to clear any existing metadata
-    PANEL.html("");
-
-    // Use `Object.entries` to add each key and value pair to the panel
-    // Hint: Inside the loop, you will need to use d3 to append new
-    // tags for each key-value in the metadata.
-    Object.entries(result).forEach(([key, value]) => {
-      PANEL.append("h6").text(`${key.toUpperCase()}: ${value}`);
-    });
-
-  });
-}
-
-// 1. Create the buildCharts function.
-function buildCharts(sample) {
-  // 2. Use d3.json to load and retrieve the samples.json file 
-  d3.json("samples.json").then((data) => {
-    // 3. Create a variable that holds the samples array. 
-    var samples = data.samples;
-    
-
-    var samplesArray = samples.sort((a,b) => a.otu_ids - b.otu_ids).reverse();
-
-    // 4. Create a variable that filters the samples for the object with the desired sample number.
-    var topTen = samplesArray.slice(0,11);
-
-    //  5. Create a variable that holds the first sample in the array.
-    var first = d3.select("#bar");
-
-    // 6. Create variables that hold the otu_ids, otu_labels, and sample_values.
-    var otuIds = samplesArray.map(samples => samples.otu_ids)
-    var otuLabels = samplesArray.map(samples => samples.otu_lables)
-    var sampleValues = samplesArray.map(samples => samples.sample_values)
-
-    // 7. Create the yticks for the bar chart.
-    // Hint: Get the the top 10 otu_ids and map them in descending order  
-    //  so the otu_ids with the most bacteria are last. 
-
-    var yticks = topTen
-
-    // 8. Create the trace for the bar chart. 
-    var barData = [{
-      x: otuLabels,
-      y: sampleValues,
-      type: "bar"      
-    }];
-    // 9. Create the layout for the bar chart. 
-    var barLayout = {
-     title: "Top 1- Bacteria Cultures Founds",
-     xaxis: {title: "Bacteria Names"},
-     yaxis: {title: "Number of Cultures"}
-
-    };
-    // 10. Use Plotly to plot the data with the layout. 
-    Plotly.newPlot("bar", barData, barLayout);
-  });
-}
+// Render the plot to the div tag with id "plot"
+Plotly.newPlot("plot", data, layout);
+console.log(data)
